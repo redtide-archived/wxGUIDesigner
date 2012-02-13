134f5a28e4af2212edaca3056f3ca3e692381604
AZ <wxguidesigner@gmail.com>
2012-02-13 16:22:39 +0100

    Updated premake_gen.bat

--- premake_gen.bat
+++ premake_gen.bat
@@ -5,7 +5,7 @@ REM ##  Author:      Andrea Zanellato
 REM ##  Modified by: 
 REM ##  Created:     2011/10/22
 REM ##  Copyright:   (c) Andrea Zanellato
-REM ##  Licence:     GNU General Public License Version 2
+REM ##  Licence:     wxWidgets licence
 REM ############################################################################
 @ECHO OFF
 
@@ -13,59 +13,72 @@ REM Clear output
 CLS
 
 REM Set Defaults
-SET unicode=
+SET wxcompiler=gcc
+SET wxmedia=yes
+SET wxmonolithic=no
 SET wxroot=%WXWIN%
-SET wxver=2.9
-SET compiler=gcc
+SET wxstatic=no
+SET wxunicode=yes
+SET wxversion=2.9
 
 REM Handle parameters
 :Loop
 REM Show help and exit
-IF [%1]==[-h]                   GOTO Help
-IF [%1]==[--help]               GOTO Help
-IF [%1]==[--compiler]           GOTO Compiler
-IF [%1]==[--disable-mediactrl]  GOTO Mediactrl
-IF [%1]==[--wx-root]            GOTO Root
-IF [%1]==[--disable-unicode]    GOTO Unicode
-IF [%1]==[--wx-version]         GOTO Version
+IF [%1]==[-h]               GOTO Help
+IF [%1]==[--help]           GOTO Help
+IF [%1]==[--wxcompiler]     GOTO Compiler
+IF [%1]==[--wxmedia]        GOTO Media
+IF [%1]==[--wxmonolithic]   GOTO Monolithic
+IF [%1]==[--wxstatic]       GOTO Static
+IF [%1]==[--wxroot]         GOTO Root
+IF [%1]==[--wxunicode]      GOTO Unicode
+IF [%1]==[--wxversion]      GOTO Version
 GOTO Premake
 
 :Help
 ECHO.
 ECHO Available options:
 ECHO.
-ECHO --compiler             Specify compiler used.
-ECHO                        Example: --compiler=vc if you use MSVC.
-ECHO                        Current: %compiler%
+ECHO --wxcompiler   Specify the compiler to use.
+ECHO                Example: --wxcompiler=vc if you use MSVC.
+ECHO                Current: %wxcompiler%
 ECHO.
-ECHO --disable-mediactrl    Disable wxMediaCtrl / wxMedia library.
-ECHO                        Default: wxMediaCtrl enabled.
+ECHO --wxmedia      Disable wxMedia library (wxMediaCtrl).
+ECHO                Example: --wxmedia=no to disable it.
+ECHO                Default: %wxmedia%
 ECHO.
-ECHO --disable-unicode      Whether to use an Unicode or an ANSI build.
-ECHO                        Ignored in wxWidgets 2.9 and later.
-ECHO                        Example: --disable-unicode produces an ANSI build.
-ECHO                        Default: Unicode build on all versions.
+ECHO --wxunicode    Whether to use an Unicode or an ANSI build.
+ECHO                Ignored in wxWidgets 2.9 and later.
+ECHO                Example: --wxunicode=no produces an ANSI build.
+ECHO                Default: %wxunicode%
 ECHO.
-ECHO --wx-root              Specify the wxWidgets build path,
-ECHO                        useful for wxWidgets builds not installed
-ECHO                        in your system (alternate/custom builds)
-ECHO                        Example: --wx-root=D:\Devel\wxWidgets\3.0
-ECHO                        Current: %WXWIN%
+ECHO --wxroot       Specify the wxWidgets build path,
+ECHO                useful for wxWidgets builds not installed
+ECHO                in your system (alternate/custom builds)
+ECHO                Example: --wxroot=D:\Devel\wxWidgets\3.0
+ECHO                Current: %WXWIN%
 ECHO.
-ECHO --wx-version           Specify the wxWidgets version.
-ECHO                        Example: --wx-version=3.0
-ECHO                        Default: %wxver%
+ECHO --wxversion    Specify the wxWidgets version.
+ECHO                Example: --wxversion=3.0
+ECHO                Default: %wxversion%
 ECHO.
 GOTO End
 
 :Compiler
-SET compiler=%2
+SET wxcompiler=%2
 SHIFT
 SHIFT
 GOTO Loop
 
-:Mediactrl
-SET mediactrl=%1
+:Media
+SET wxmedia=%2
+SHIFT
+SHIFT
+GOTO Loop
+
+:Monolithic
+SET wxmonolithic=%2
+SHIFT
 SHIFT
 GOTO Loop
 
@@ -75,22 +88,29 @@ SHIFT
 SHIFT
 GOTO Loop
 
+:Static
+SET wxstatic=%2
+SHIFT
+SHIFT
+GOTO Loop
+
 :Unicode
-SET unicode=%1
+SET wxunicode=%2
+SHIFT
 SHIFT
 GOTO Loop
 
 :Version
-SET wxver=%2
+SET wxversion=%2
 SHIFT
 SHIFT
 GOTO Loop
 
 :Premake
-build\premake\windows\premake4.exe --file=build/premake/solution.lua --wx-root=%wxroot% --wx-version=%wxver% --compiler=%compiler% %unicode% %mediactrl% codelite
+build\premake\windows\premake4.exe --file=build/premake/solution.lua --wxcompiler=%wxcompiler% --wxmedia=%wxmedia% --wxmonolithic=%wxmonolithic% --wxroot=%wxroot% --wxstatic=%wxstatic% --wxunicode=%wxunicode% --wxversion=%wxversion% codelite
 ECHO.
 
-build\premake\windows\premake4.exe --file=build/premake/solution.lua --wx-root=%wxroot% --wx-version=%wxver% --compiler=%compiler% %unicode% %mediactrl% vs2008
+REM build\premake\windows\premake4.exe --file=build/premake/solution.lua --wxcompiler=%wxcompiler% --wxmedia=%wxmedia% --wxmonolithic=%wxmonolithic% --wxroot=%wxroot% --wxstatic=%wxstatic% --wxunicode=%wxunicode% --wxversion=%wxversion% vs2008
 ECHO.
 
 :End
