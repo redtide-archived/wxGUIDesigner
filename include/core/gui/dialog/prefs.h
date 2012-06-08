12bf33274fb1c72688effcc8928104a67b77a92b
AZ <wxguidesigner@gmail.com>
2012-06-08 23:25:17 +0200

    Added wxGDPageEditors

--- include/core/gui/dialog/prefs.h
+++ include/core/gui/dialog/prefs.h
@@ -13,8 +13,10 @@
 class wxBitmapComboBox;
 class wxButton;
 class wxCheckBox;
+class wxChoice;
 class wxDialog;
 class wxPanel;
+class wxSpinCtrl;
 class wxStaticText;
 class wxStdDialogButtonSizer;
 class wxTreebook;
@@ -26,8 +28,8 @@ class wxUpdateUIEvent;
 class wxGDHandler;
 
 wxDECLARE_EVENT( wxGD_EVT_GUI_OPTION_CHANGED, wxCommandEvent );
-wxDECLARE_EVENT( wxGD_EVT_GUI_CONFIG_UPDATE, wxCommandEvent );
-wxDECLARE_EVENT( wxGD_EVT_GUI_CONFIG_SAVE,   wxCommandEvent );
+wxDECLARE_EVENT( wxGD_EVT_GUI_CONFIG_UPDATE,  wxCommandEvent );
+wxDECLARE_EVENT( wxGD_EVT_GUI_CONFIG_SAVE,    wxCommandEvent );
 //=============================================================================
 // wxGDDialogPrefs
 //=============================================================================
@@ -71,6 +73,33 @@ private:
 
     bool m_enabled;
     int  m_selected;
+};
+//=============================================================================
+// wxGDPageEditors
+//=============================================================================
+class wxGDPageEditors : public wxPanel 
+{
+public:
+    wxGDPageEditors( wxGDHandler *handler, wxGDDialogPrefs *parent ); 
+    ~wxGDPageEditors();
+
+private:
+    void OnPrefsChanged ( wxCommandEvent &event );
+    void OnUpdatePrefs  ( wxCommandEvent &event );
+    void OnSavePrefs    ( wxCommandEvent &event );
+
+    wxGDHandler      *m_handler;
+    wxBitmapComboBox *m_bcbEditor;
+    wxCheckBox       *m_chkLineNums;
+    wxCheckBox       *m_chkGuides;
+    wxCheckBox       *m_chkWSpace;
+    wxCheckBox       *m_chkEOL;
+    wxCheckBox       *m_chkBackspace;
+    wxCheckBox       *m_chkUseTabs;
+    wxCheckBox       *m_chkTabsIndent;
+    wxChoice         *m_choTabsWidth;
+    wxSpinCtrl       *m_spnCaretW;
+
 };
 
 #endif //__WXGD_GUI_DIALOG_PREFS_H__
--- src/core/gui/dialog/prefs.cpp
+++ src/core/gui/dialog/prefs.cpp
@@ -10,11 +10,13 @@
 #include <wx/bmpcbox.h>
 #include <wx/button.h>
 #include <wx/checkbox.h>
+#include <wx/choice.h>
 #include <wx/config.h>
 #include <wx/dialog.h>
 #include <wx/imaglist.h>
 #include <wx/panel.h>
 #include <wx/sizer.h>
+#include <wx/spinctrl.h>
 #include <wx/statbox.h>
 #include <wx/stattext.h>
 #include <wx/treebook.h>
@@ -25,8 +27,8 @@
 #include "core/gui/handler.h"
 
 wxDEFINE_EVENT( wxGD_EVT_GUI_OPTION_CHANGED, wxCommandEvent );
-wxDEFINE_EVENT( wxGD_EVT_GUI_CONFIG_UPDATE, wxCommandEvent );
-wxDEFINE_EVENT( wxGD_EVT_GUI_CONFIG_SAVE,   wxCommandEvent );
+wxDEFINE_EVENT( wxGD_EVT_GUI_CONFIG_UPDATE,  wxCommandEvent );
+wxDEFINE_EVENT( wxGD_EVT_GUI_CONFIG_SAVE,    wxCommandEvent );
 //=============================================================================
 // wxGDDialogPrefs
 //=============================================================================
@@ -51,18 +53,24 @@ m_tbkPrefs  ( NULL )
     m_tbkPrefs             = new wxTreebook( this, wxID_ANY  );
     m_tbkPrefs->AssignImageList( imageList );
 
-    wxBitmap bmpLocale  = wxXmlResource::Get()->LoadBitmap("language");
-    wxBitmap bmpDesign  = wxXmlResource::Get()->LoadBitmap("designer");
+    wxBitmap bmpLocale  = wxXmlResource::Get()->LoadBitmap("locale");
+//  wxBitmap bmpDesign  = wxXmlResource::Get()->LoadBitmap("designer");
+    wxBitmap bmpEditor  = wxXmlResource::Get()->LoadBitmap("editor");
 
     int imgIndex = -1;
     if( bmpLocale.IsOk() )
         imgIndex = imageList->Add( bmpLocale );
     m_tbkPrefs->AddPage( new wxGDPageLocale( m_handler, this ), _("Locale"), true, imgIndex );
-
+/*
     imgIndex = -1;
     if( bmpDesign.IsOk() )
         imgIndex = imageList->Add( bmpDesign );
     m_tbkPrefs->AddPage( new wxPanel( this, wxID_ANY ), _("GUI"), false, imgIndex );
+*/
+    imgIndex = -1;
+    if( bmpEditor.IsOk() )
+        imgIndex = imageList->Add( bmpEditor );
+    m_tbkPrefs->AddPage( new wxGDPageEditors( m_handler, this ), _("Editors"), false, imgIndex );
 
     mainSizer->Add( m_tbkPrefs, 1, wxALL|wxEXPAND, 5 );
 
@@ -123,7 +131,6 @@ void wxGDDialogPrefs::OnSavePrefs( wxCommandEvent & )
 
     Destroy();
 }
-
 //=============================================================================
 // wxGDPageLocale
 //=============================================================================
@@ -237,4 +244,164 @@ void wxGDPageLocale::OnSavePrefs( wxCommandEvent & )
     wxConfigBase::Get()->Write( "locale/enabled",  m_chkLang->IsChecked() );
     wxConfigBase::Get()->Write( "locale/selected", m_bcbLang->GetSelection() );
     wxConfigBase::Get()->Flush();
+}
+//=============================================================================
+// wxGDPageEditors
+//=============================================================================
+wxGDPageEditors::wxGDPageEditors( wxGDHandler *handler, wxGDDialogPrefs *parent )
+:
+wxPanel( parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL ),
+m_handler       ( handler ),
+m_bcbEditor     ( NULL ),
+m_chkLineNums   ( NULL ),
+m_chkGuides     ( NULL ),
+m_chkWSpace     ( NULL ),
+m_chkEOL        ( NULL ),
+m_chkBackspace  ( NULL ),
+m_chkUseTabs    ( NULL ),
+m_chkTabsIndent ( NULL ),
+m_choTabsWidth  ( NULL ),
+m_spnCaretW     ( NULL )
+{
+    wxBoxSizer* mainSizer = new wxBoxSizer( wxVERTICAL );
+
+    wxFlexGridSizer* editSizer = new wxFlexGridSizer( 0, 2, 0, 0 );
+    editSizer->AddGrowableCol( 1 );
+    editSizer->SetFlexibleDirection( wxBOTH );
+    editSizer->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+
+    wxStaticText* lblEditor;
+    lblEditor = new wxStaticText( this, wxID_ANY, _("Editor:"), wxDefaultPosition, wxDefaultSize, 0 );
+    lblEditor->Wrap( -1 );
+    editSizer->Add( lblEditor, 0, wxALIGN_CENTER_VERTICAL|wxLEFT|wxTOP, 5 );
+
+    m_bcbEditor = new wxBitmapComboBox( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
+    editSizer->Add( m_bcbEditor, 0, wxEXPAND|wxRIGHT|wxTOP, 5 );
+
+    mainSizer->Add( editSizer, 0, wxEXPAND, 5 );
+
+    wxStaticBoxSizer* sbsGuides;
+    sbsGuides = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, _("Guides") ), wxVERTICAL );
+
+    wxPanel* pnlGuides;
+    pnlGuides = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+    wxFlexGridSizer* fgsGuides;
+    fgsGuides = new wxFlexGridSizer( 0, 2, 0, 0 );
+    fgsGuides->AddGrowableCol( 1 );
+    fgsGuides->SetFlexibleDirection( wxBOTH );
+    fgsGuides->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+
+    m_chkLineNums = new wxCheckBox( pnlGuides, wxID_ANY, _("Show Line Numbers"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsGuides->Add( m_chkLineNums, 0, 0, 5 );
+
+    m_chkGuides = new wxCheckBox( pnlGuides, wxID_ANY, _("Show Indentation Guides"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsGuides->Add( m_chkGuides, 0, 0, 5 );
+
+    m_chkWSpace = new wxCheckBox( pnlGuides, wxID_ANY, _("View Whitespace"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsGuides->Add( m_chkWSpace, 0, 0, 5 );
+
+    m_chkEOL = new wxCheckBox( pnlGuides, wxID_ANY, _("View EOL"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsGuides->Add( m_chkEOL, 0, 0, 5 );
+
+    m_chkBackspace = new wxCheckBox( pnlGuides, wxID_ANY, _("Backspace Unindents"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsGuides->Add( m_chkBackspace, 0, 0, 0 );
+
+    pnlGuides->SetSizer( fgsGuides );
+    pnlGuides->Layout();
+    fgsGuides->Fit( pnlGuides );
+    sbsGuides->Add( pnlGuides, 0, wxEXPAND, 5 );
+
+    mainSizer->Add( sbsGuides, 0, wxEXPAND|wxLEFT|wxRIGHT, 5 );
+
+    wxStaticBoxSizer* sbsTabs;
+    sbsTabs = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, _("Tabs") ), wxVERTICAL );
+
+    wxPanel* pnlTabs;
+    pnlTabs = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+    wxFlexGridSizer* fgsTabs;
+    fgsTabs = new wxFlexGridSizer( 0, 2, 0, 0 );
+    fgsTabs->AddGrowableCol( 1 );
+    fgsTabs->SetFlexibleDirection( wxBOTH );
+    fgsTabs->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+
+    m_chkUseTabs = new wxCheckBox( pnlTabs, wxID_ANY, _("Use Tabs"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsTabs->Add( m_chkUseTabs, 0, 0, 5 );
+
+    m_chkTabsIndent = new wxCheckBox( pnlTabs, wxID_ANY, _("Tabs Indent"), wxDefaultPosition, wxDefaultSize, 0 );
+    fgsTabs->Add( m_chkTabsIndent, 0, wxLEFT, 5 );
+
+    wxStaticText* lblTabsWidth;
+    lblTabsWidth = new wxStaticText( pnlTabs, wxID_ANY, _("Tabs Width:"), wxDefaultPosition, wxDefaultSize, 0 );
+    lblTabsWidth->Wrap( -1 );
+    fgsTabs->Add( lblTabsWidth, 0, wxALIGN_CENTER_VERTICAL, 5 );
+
+    wxString m_choTabsWidthChoices[] = { _("1"), _("2"), _("4"), _("8") };
+    int m_choTabsWidthNChoices = sizeof( m_choTabsWidthChoices ) / sizeof( wxString );
+    m_choTabsWidth = new wxChoice( pnlTabs, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choTabsWidthNChoices, m_choTabsWidthChoices, 0 );
+    m_choTabsWidth->SetSelection( 2 );
+    fgsTabs->Add( m_choTabsWidth, 0, wxEXPAND, 5 );
+
+    pnlTabs->SetSizer( fgsTabs );
+    pnlTabs->Layout();
+    fgsTabs->Fit( pnlTabs );
+    sbsTabs->Add( pnlTabs, 0, wxEXPAND, 0 );
+
+    mainSizer->Add( sbsTabs, 0, wxEXPAND|wxLEFT|wxRIGHT, 5 );
+
+    wxStaticBoxSizer* sbsMisc;
+    sbsMisc = new wxStaticBoxSizer( new wxStaticBox( this, wxID_ANY, _("Misc") ), wxVERTICAL );
+
+    wxPanel* pnlMisc;
+    pnlMisc = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+    wxBoxSizer* sizerMisc;
+    sizerMisc = new wxBoxSizer( wxVERTICAL );
+
+    wxFlexGridSizer* fgsMisc;
+    fgsMisc = new wxFlexGridSizer( 0, 2, 0, 0 );
+    fgsMisc->AddGrowableCol( 1 );
+    fgsMisc->SetFlexibleDirection( wxBOTH );
+    fgsMisc->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+
+    wxStaticText* lblCaretW;
+    lblCaretW = new wxStaticText( pnlMisc, wxID_ANY, _("Caret Width:"), wxDefaultPosition, wxDefaultSize, 0 );
+    lblCaretW->Wrap( -1 );
+    fgsMisc->Add( lblCaretW, 0, wxALL, 5 );
+
+    m_spnCaretW = new wxSpinCtrl( pnlMisc, wxID_ANY, wxT("1"), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 1, 10, 1 );
+    fgsMisc->Add( m_spnCaretW, 1, wxEXPAND, 5 );
+
+    sizerMisc->Add( fgsMisc, 0, wxEXPAND, 5 );
+
+    pnlMisc->SetSizer( sizerMisc );
+    pnlMisc->Layout();
+    sizerMisc->Fit( pnlMisc );
+    sbsMisc->Add( pnlMisc, 1, wxEXPAND, 0 );
+
+    mainSizer->Add( sbsMisc, 1, wxBOTTOM|wxEXPAND|wxLEFT|wxRIGHT, 5 );
+
+    SetSizer( mainSizer );
+    Layout();
+}
+
+wxGDPageEditors::~wxGDPageEditors()
+{
+}
+
+void wxGDPageEditors::OnPrefsChanged( wxCommandEvent & )
+{
+    bool isDirty = false;
+
+    wxCommandEvent evt( wxGD_EVT_GUI_OPTION_CHANGED );
+    evt.SetInt( isDirty ? 1 : 0 );
+    GetParent()->GetEventHandler()->ProcessEvent( evt );
+}
+
+void wxGDPageEditors::OnUpdatePrefs( wxCommandEvent & )
+{
+
+}
+
+void wxGDPageEditors::OnSavePrefs( wxCommandEvent & )
+{
+    
 }
